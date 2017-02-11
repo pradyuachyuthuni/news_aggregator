2497d0a74c24014ac413d83144d8ba6ea0549a5e
smahadevabatla <smahadevabatla@cavium.com>
2017-02-10 21:22:03 -0500

    database connections

--- web2txt.py
+++ web2txt.py
@@ -9,8 +9,11 @@ from bs4 import BeautifulSoup
 
 import process_url
 
-database = []
-# an example would be database = [[heading1,content1],[heading2,content2],[heading3,content3],...]
+database_name = 'news.db'
+connection = sqlite3.connect(database_name)
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE news (subject text, article text)''') #initialize database
+
 
 def striphtml(data):
     p = re.compile(r'<.*?>')
@@ -30,34 +33,39 @@ def get_news(url):
     news = news[1:len(news)-1]  # think of a way to remove this. it consumes memory. debug why you are getting square brackets in the first place.
     return news
 
-def get_news_urls(index_string):
+def get_news_and_store_in_database(index_string):
+    #global connection
+    #global cursor
+    count = 1
     requests_cache.install_cache()
     page = requests.get(index_string)
     soup = BeautifulSoup(page.content, 'html.parser')
     for line in soup.find_all('td'):
       entry = line.find_all('a')
       for news in entry:
-        if '/news/politics-and-nation/' not in str(news.get('href')):
-           continue
         if news.find_all('b'):
             subject = '# find a way to get the month here'
         else:
             subject = news.contents[0]
         url = 'http://economictimes.indiatimes.com' + news.get('href')
         content = get_news(url)
-        database.append([url,subject,content])
-
+        count += 1
+        print(subject,content)
+        cursor.execute("INSERT INTO news VALUES (?,?)", (subject,content))
+        connection.commit()
+        if count > 5:
+           connection.close()
+            
 
 def get_and_process_index_string_for_news_urls():
     index_string_list = process_url.process_index_string()
     for index_string in index_string_list:
-        get_news_urls(index_string)
+        get_news_and_store_in_database(index_string)
 
 
 def start():
     get_and_process_index_string_for_news_urls()
-    print(len(database))
-
+       
 
 if __name__ == '__main__':
     start()
