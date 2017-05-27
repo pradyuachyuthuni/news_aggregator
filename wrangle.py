c022836e28275f0f49d1ba4d6d672d680edc47b7
root <pradyuachyuthuni@gmail.com>
2017-05-27 17:24:42 +0530

    Modified wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,6 @@
 import os
 import time
-import re
+from django.utils.html import strip_tags
 
 class NewsObject:
   def __init__(self, title=None, author=None, date_and_time=None, article_link=None, image_path=None, content=None):
@@ -11,14 +11,9 @@ class NewsObject:
     self.image_path = image_path
     self.content = content
 
-  def print_news_capsule(self):
-    print self.title 
-    print self.author
-    print self.date_and_time
-    print self.article_link
-    print self.image_path 
-    print self.content
-    print "------------------------- END OF OBJECT -------------------------"
+  def write_news_capsule_to_file(self):
+    fd.write(strip_tags(str(self.__dict__)))
+    fd.write('\n')
 
 def create_raw_data_file():
     input_file = 'april.log'
@@ -42,20 +37,22 @@ def instantiate_news_objects(news_list):
      object_internal_separator = '|'
      obj_contents_list = news_item.split(object_internal_separator)
      length = len(obj_contents_list)
-       if length is 5:
-        title,article_link,date_and_time,image_path,content = obj_contents_list
-        author = "Inhouse"
-       elif length is 6:
-        title,article_link,author,date_and_time,image_path,content = obj_contents_list
-       elif length > 6:
-        title,article_link,author,date_and_time,image_path = obj_contents_list[0:5]
-        content = obj_contents_list[5:]
-       else:
-        print "improper news object found"
+     if length is 5:
+      title,article_link,date_and_time,image_path,content = obj_contents_list
+      author = "Inhouse"
+     elif length is 6:
+      title,article_link,author,date_and_time,image_path,content = obj_contents_list
+     elif length > 6:
+      title,article_link,author,date_and_time,image_path = obj_contents_list[0:5]
+      content = obj_contents_list[5:]
+     else:
+      print "improper news object found"
      news_object = NewsObject(title,author,date_and_time,article_link,image_path,content)
-     news_object.print_news_capsule()
+     news_object.write_news_capsule_to_file()
 
 if __name__ == '__main__':
+    fd = open('news_database.txt','w',0)
     raw_data_file = create_raw_data_file()
     news_list = cleanup_unwanted_data(raw_data_file)
     instantiate_news_objects(news_list)
+    
